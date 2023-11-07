84f59edaceb41aed81c033e9d7a274e84d70ef54
MagedKhaled <maged.khaled03@gmail.com>
2023-11-07 03:21:37 +0200

    product details page without suggestion section

--- front_end/src/Router/router.js
+++ front_end/src/Router/router.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-const Loader = React.lazy(() => import("../general_components/Loader/Loader"));
+import TextDetails from "../pages/Details/text_details";
+const Loader = React.lazy(() => import("../general_general_components/Loader/Loader"));
 const Cart = React.lazy(() => import("../pages/Cart/Cart"));
 const NotFound = React.lazy(() => import("../pages/NotFound"));
 
@@ -10,8 +11,10 @@ function Router() {
   return (
     <Suspense fallback={<Loader />}>
       <Routes>
+      <Route path="/" element={<TextDetails />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="*" element={<NotFound />} />
+
       </Routes>
     </Suspense>
   );

600c6b9c1ede81fd8161b9c8d0d8a5812ff65cce
MagedKhaled <maged.khaled03@gmail.com>
2023-11-13 17:50:49 +0200

    befor merging

--- front_end/src/pages/Order/componants/TotalPrice.js
+++ front_end/src/pages/Order/componants/TotalPrice.js
@@ -3,7 +3,8 @@ export default function TotalPrice({price}){
         <div className="pe-5">
             <div className="d-flex flex-row justify-content-end">
                 <h3>Total Price: </h3>
-                <h3>{price}</h3>
+                <h3>{1800}</h3>
+                {/* <h3>{price}</h3> */}
             </div>
         </div>
     )

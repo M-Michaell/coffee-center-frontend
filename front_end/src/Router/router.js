642878269140886c4a6814b2b86bcbcf2c9d0722
mohammed mostafa <mm994225@gmail.com>
2023-11-18 02:44:51 +0200

    modfy router

--- front_end/src/Router/router.js
+++ front_end/src/Router/router.js
@@ -1,25 +1,18 @@
 // Router.js
 import React, {Suspense} from "react";
 import {Route, Routes} from "react-router-dom";
-
-
 import DeleteUser from "../pages/delete_user/DeleteUser";
 const ActivatePage= React.lazy(()=> import("../pages/registration/Activation"));
 const SendMail= React.lazy(()=> import("../pages/registration/SendMail"));
 const Reset= React.lazy(()=> import("../pages/registration/Reset"));
 const Order= React.lazy(()=> import("../pages/Order/OrderPage"));
 const TextDetails= React.lazy(()=> import("../pages/Details/text_details"));
-
-
-
 const Home = React.lazy(() => import("../pages/Home/Home"));
 const Loader = React.lazy(() => import("../general_components/Loader/Loader"));
 const Cart = React.lazy(() => import("../pages/Cart/Cart"));
 const Login = React.lazy(() => import("../pages/Auth/Login"));
 const NotFound = React.lazy(() => import("../pages/NotFound"));
-const Registration = React.lazy(() =>
-    import("../pages/registration/Registration")
-);
+const Registration = React.lazy(() => import("../pages/registration/Registration"));
 const Account = React.lazy(() => import("../pages/Profile/ProfilePage"));
 const Search = React.lazy(() => import("../pages/Search/component/Search"));
 const AddressForm = React.lazy(() => import("../pages/Profile/component/AddressForm"));
@@ -31,7 +24,6 @@ function Router() {
             <Routes>
                 <Route path="/product/details/" element={<TextDetails/>}/>
                 <Route path="/cart" element={<Cart/>}/>
-
                 <Route path="/home" element={<Home/>}/>
                 <Route path="/activate/:uid/:token" element={<ActivatePage/>}/>
                 <Route path="/reset-password" element={<SendMail/>}/>

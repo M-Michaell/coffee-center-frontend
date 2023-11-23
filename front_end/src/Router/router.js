c91ed0e4b50f39ca50043add4d397278d775fe86
ShazaShaban <shathashabanalberry@gmail.com>
2023-11-23 06:21:57 +0200

    add admin dashboard base,fix forms validation&othr

--- front_end/src/Router/router.js
+++ front_end/src/Router/router.js
@@ -59,6 +59,12 @@ function Router() {
                         <Account/>
                     </PrivateRoute>
                     }/>
+                <Route path="/admin/*" element={
+                    <PrivateRoute>
+                      <AdminDashboard />
+                    </PrivateRoute>
+                    }
+                    />
                 <Route path="/order/tracking/" element={<Account/>}/>
                 <Route path="/search/:productname" element={<Search/>}/>
                 <Route path="*" element={<NotFound/>}/>
@@ -66,7 +72,7 @@ function Router() {
                 <Route path="registration/" element={<Registration/>}/>
                 <Route path="login/" element={<Login/>}/>
                 <Route path="/profile/*" element={<Account/>}/>
-                <Route path="/addressform/:id" element={<AddressForm/>}/>
+                <Route path="/addressform/:id" element={<AddressEdit/>}/>
                 <Route path="/addressform" element={<AddressForm/>}/>
                 <Route path="/paypal/" element={<Checkout />} />
                 <Route path="/add-product" element={<Creator/>}/>

4c7f72341751e20864fb55c6279b05fd2cd81a79
mohammed mostafa <mm994225@gmail.com>
2023-11-30 11:03:33 +0200

    modfy user staff

--- front_end/src/pages/Auth/PrivateAdmin.js
+++ front_end/src/pages/Auth/PrivateAdmin.js
@@ -1,6 +1,8 @@
 import {Navigate, NavLink} from "react-router-dom";
+import {useSelector} from "react-redux";
 
 export default function PrivateAdmin({children}){
-    const user = localStorage.getItem("userInfo");
-    return user.is_stuff ? children : <Navigate to='/*'/>
+     const user = useSelector((state) => state.auth.userInfo);
+
+    return  user.is_staff? children : <Navigate to='/home'/>
 }
